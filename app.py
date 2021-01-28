3d6faba48a5467694324af3fad198b91b92d3c83
prakashPC <pks957@gmail.com>
2021-01-28 15:58:39 +0530

    bug fixed : not working for if all vol ratio are below 1

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from calculator import get_table
 from outlook_mail import send_mail
 from datetime import datetime
 import pytz
-from sql_functions import  insert_df, read_sql_db, delete_table
+from sql_functions import insert_df, read_sql_db, delete_table
 from metadata import *
 
-
 IST = pytz.timezone('Asia/Kolkata')
+
 
 def checking():
     if datetime.now(IST).strftime('%A') in weekends: return 0
@@ -14,10 +14,10 @@ def checking():
         delete_table('high_volume_stocks')
     else:
         df, max_ratio_date = get_table(3)
-        if max_ratio_date == datetime.now(IST).strftime(r'%Y-%m-%d'): 
+        if max_ratio_date == datetime.now(IST).strftime(r'%Y-%m-%d'):
             df['query_time'] = datetime.now(IST).strftime('%H:%M:%S')
-            insert_df(df,'high_volume_stocks',column_names = column_names, column_dtypes = column_dtypes)
-            sql_df = read_sql_db('high_volume_stocks',column_names=column_names)
+            insert_df(df, 'high_volume_stocks', column_names=column_names, column_dtypes=column_dtypes)
+            sql_df = read_sql_db('high_volume_stocks', column_names=column_names)
             print(df, sql_df)
             if len(sql_df):
                 send_mail(sql_df)
@@ -26,5 +26,3 @@ def checking():
 if __name__ == '__main__':
     checking()
     # print(datetime.now(IST).strftime('%H'))
-
-
--- calculator.py
+++ calculator.py
@@ -2,33 +2,36 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 
-#read Yahoo codes from file
-companies = pd.read_csv('Yahoo_Company_codes.csv', names=['yahoocd', 'Description'],skiprows=1)
+# read Yahoo codes from file
+companies = pd.read_csv('Yahoo_Company_codes.csv', names=['yahoocd', 'Description'], skiprows=1)
 
 # Dropping Description column is all values are null
 if companies.Description.notnull().sum() == 0: companies.drop('Description', axis=1, inplace=True)
 
-#long string of all yahoo codes as they can be processed at once 
+# long string of all yahoo codes as they can be processed at once
 yahoo_tickers = yf.Tickers(' '.join([x for x in companies.yahoocd.values]))
 
 
-#function to calculate volume ratio 
+# function to calculate volume ratio
 def get_latest_volume_ratio(yahoo_ticker, period='1mo'):
-    ticker_last_X_days = yahoo_ticker.history(period=period)[['Close','Volume']]
+    ticker_last_X_days = yahoo_ticker.history(period=period)[['Close', 'Volume']]
     volumes = np.array(ticker_last_X_days.Volume[-2:])
     return float(volumes[-1] / volumes[-2]), max(ticker_last_X_days.index)
 
 
-#return a df for alll yahoo codes  
-def get_table(volume_ratio_cutoff, df = companies):
-    rsi_volume = pd.DataFrame([get_latest_volume_ratio(ticker) for ticker in yahoo_tickers.tickers], columns=['vol_ratio','ratio_date']).reindex(df.index)
-    df = pd.concat([df, rsi_volume],axis=1 )
+# return a df for alll yahoo codes
+def get_table(volume_ratio_cutoff, df=companies):
+    rsi_volume = pd.DataFrame([get_latest_volume_ratio(ticker) for ticker in yahoo_tickers.tickers],
+                              columns=['vol_ratio', 'ratio_date']).reindex(df.index)
+    df = pd.concat([df, rsi_volume], axis=1)
     df['action'] = df.vol_ratio.apply(lambda x: 'BUY/SELL' if x > volume_ratio_cutoff else np.nan)
-    df = df[df.vol_ratio > volume_ratio_cutoff ]
-    return df, max(df.ratio_date.dt.strftime('%Y-%m-%d'))
+    max_date =  max(df.ratio_date.dt.strftime('%Y-%m-%d'))
+    df = df[df.vol_ratio > volume_ratio_cutoff]
+    return df, max_date
+    # print(df)
 
 # #for testing this code should be commented before importing
 # if __name__ == '__main__':
 #     # timea = time.time()
-#     print(get_table(1))
-#     # print(f'Total Time taken : {time.time()-timea}')    
+#     print(get_table(2))
+#     # print(f'Total Time taken : {time.time()-timea}')
